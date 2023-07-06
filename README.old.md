fd9d27a5e075051405b3e25da0f0786ca5abd887
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-06 19:52:48 +0900

    add firebase bsic setting is finiesed

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# React_Firebase_BlogApp
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzey0xLoDpSCWGAXEDMvD4u8M1s9RX36s",
+  authDomain: "blog-196fe.firebaseapp.com",
+  projectId: "blog-196fe",
+  storageBucket: "blog-196fe.appspot.com",
+  messagingSenderId: "551820892332",
+  appId: "1:551820892332:web:bac4d6830b9726cf6da709",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
